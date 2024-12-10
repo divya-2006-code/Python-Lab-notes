b35dcd041e258af65c69d96894a8ed806192f5bb
divya-2006-code <divyasara2006@gmail.com>
2024-12-10 14:13:47 +0530

    Add files via upload

---
+++ Factorial using recursion (5).py
@@ -0,0 +1,12 @@
+#Factorial Using Recursion:
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n*factorial(n - 1)
+
+num = int(input("Enter the number:"))
+print(f"The factorial is {factorial(num)}")
+        
+    
