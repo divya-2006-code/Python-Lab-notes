c472ea25c673637c4e28ea18ce3c7343f48ce2ab
divya-2006-code <divyasara2006@gmail.com>
2024-12-11 23:17:07 +0530

    Add files via upload

---
+++ Count uppercase and lowercase letter (7).py
@@ -0,0 +1,17 @@
+def count_case_letters(text):
+    upper_count = 0
+    lower_count = 0
+
+    for char in text:
+        if char.isupper():
+            upper_count += 1
+        elif char.islower():
+            lower_count += 1
+
+    return upper_count, lower_count
+
+ 
+text = input("Enter a string: ")
+upper, lower = count_case_letters(text)
+print(f"Number of uppercase letters: {upper}")
+print(f"Number of lowercase letters: {lower}")
